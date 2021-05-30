a248b2a4dcd8de7e25df451bf0f53415b74e54a8
Andy Cavatorta <hellyeah@media.mit.edu>
2021-05-29 20:35:01 -0400

    preparing for first run

--- client_example.py
+++ client_example.py
@@ -8,6 +8,5 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
 def send_to_server(action, value):
-    socket.send(b"Hello")
     socket.send_json([action, value])
     print(socket.recv())
